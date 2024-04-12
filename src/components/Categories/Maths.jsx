13ce071f7efe8cc3f9bd3e67e99d1111678eccc3
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-04-12 16:05:47 +0530

    First commit

---
+++ src/components/Categories/Maths.jsx
@@ -0,0 +1,5 @@
+export default function Maths() {
+    return (
+        <h1>Maths page</h1>
+    )
+}
---
+++ src/components/Categories/Physics.jsx
@@ -0,0 +1,5 @@
+export default function Physics() {
+    return (
+        <h1>Physics page</h1>
+    )
+}
